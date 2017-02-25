444a00412621309fd45052b78bb3bdaa98f780da
sungduk kang <sungduk.kang.94@gmail.com>
2017-02-24 20:53:09 -0800

    initial

---
+++ Assets/Scripts/GripManager.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GripManager : MonoBehaviour {
+
+	public Rigidbody Body;
+
+	public Pull left;
+	public Pull right;
+
+	public float velocityMultiplier = 1.5f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () 
+	{
+		var lDevice = SteamVR_Controller.Input ((int)left.controller.index);
+		var rDevice = SteamVR_Controller.Input ((int)right.controller.index);
+
+		bool isGripped = left.canGrip || right.canGrip;
+
+		if (isGripped) 
+		{
+			if (left.canGrip && lDevice.GetTouch (SteamVR_Controller.ButtonMask.Grip)) {
+				Body.useGravity = false;
+				Body.isKinematic = true;
+				Body.transform.position += (left.prevPos - left.transform.localPosition);
+			} 
+			else if (left.canGrip && lDevice.GetTouchUp (SteamVR_Controller.ButtonMask.Grip))
+			{
+				Body.useGravity = true;
+				Body.isKinematic = false;
+				Body.velocity = ((left.prevPos - left.transform.localPosition) * velocityMultiplier) / Time.deltaTime;
+			}
+
+			if (right.canGrip && rDevice.GetTouch (SteamVR_Controller.ButtonMask.Grip)) {
+				Body.useGravity = false;
+				Body.isKinematic = true;
+				Body.transform.position += (right.prevPos - right.transform.localPosition);
+			} 
+			else if (right.canGrip && rDevice.GetTouchUp (SteamVR_Controller.ButtonMask.Grip))
+			{
+				Body.useGravity = true;
+				Body.isKinematic = false;
+				Body.velocity = ((right.prevPos - right.transform.localPosition) * velocityMultiplier) / Time.deltaTime;
+			}
+		}
+			
+		else 
+		{
+			Body.useGravity = true;
+			Body.isKinematic = false;
+		}
+
+		left.prevPos = left.transform.localPosition;
+		right.prevPos = right.transform.localPosition;
+	}
+}
---
+++ Assets/Scripts/Pull.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pull : MonoBehaviour {
+
+	public SteamVR_TrackedObject controller;
+
+	[HideInInspector]
+	public Vector3 prevPos;
+
+	[HideInInspector]
+	public bool canGrip;
+
+	// Use this for initialization
+	void Start () 
+	{
+		prevPos = controller.transform.localPosition;
+	}
+
+	void OnTriggerEnter() 
+	{
+		canGrip = true;	
+	}
+
+	void OnTriggerExit()
+	{
+		canGrip = false;
+	}
+}
