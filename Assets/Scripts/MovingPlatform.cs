262397ede09a7860261e47f39c07d2ccfae7daa5
sungdukkang <sungduk.kang.94@gmail.com>
2017-02-25 21:03:48 -0800

    tested new blocks

--- Assets/Scripts/MovingPlatform.cs
+++ Assets/Scripts/MovingPlatform.cs
@@ -8,23 +8,30 @@ public class MovingPlatform : MonoBehaviour {
 	public float distance = 1f;
 	public float speed = 1f;
 
-
-	private Vector3 origin; 
+    public Rigidbody Body;
+ 
+    private Vector3 origin; 
 
 	void Start() {
-		origin = transform.position; 
+		origin = transform.position;
 	}
 
 	void Update() {
-		if (direction.x > 0) {
-			transform.position = new Vector3 (origin.x - Mathf.PingPong (Time.time * speed, distance), origin.y, origin.z);
-		} else if (direction.y > 0) {
-			transform.position = new Vector3 (origin.x, origin.y - Mathf.PingPong (Time.time * speed, distance), origin.z);
-		} else if (direction.z > 0) {
-			transform.position = new Vector3 (origin.x, origin.y, origin.z - Mathf.PingPong (Time.time * speed, distance));
-		} else {
+        float movement = Mathf.PingPong(Time.time * speed, distance);
+        if (direction.x > 0) {
+            transform.position = new Vector3 (origin.x - movement, origin.y, origin.z);
+          
+        } else if (direction.y > 0) {
+			transform.position = new Vector3 (origin.x, origin.y - movement, origin.z);
+           
+        } else if (direction.z > 0) {
+			transform.position = new Vector3 (origin.x, origin.y, origin.z - movement);
+      
+        } else {
 			transform.position = origin;
 		}
+        
 
-	}
+    }
+
 }
