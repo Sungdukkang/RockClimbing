733c1487218fc2f25517d0e94850a218f46e40b0
sungduk kang <sungduk.kang.94@gmail.com>
2017-02-25 16:08:51 -0800

    Added different kind of blocks and a block moving script

---
+++ Assets/Scripts/MovingPlatform.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPlatform : MonoBehaviour {
+
+	public Vector3 direction; 
+	public float distance = 1f;
+	public float speed = 1f;
+
+
+	private Vector3 origin; 
+
+	void Start() {
+		origin = transform.position; 
+	}
+
+	void Update() {
+		if (direction.x > 0) {
+			transform.position = new Vector3 (origin.x - Mathf.PingPong (Time.time * speed, distance), origin.y, origin.z);
+		} else if (direction.y > 0) {
+			transform.position = new Vector3 (origin.x, origin.y - Mathf.PingPong (Time.time * speed, distance), origin.z);
+		} else if (direction.z > 0) {
+			transform.position = new Vector3 (origin.x, origin.y, origin.z - Mathf.PingPong (Time.time * speed, distance));
+		} else {
+			transform.position = origin;
+		}
+
+	}
+}
